4d08c16b34b0376d8f0cac620f899ca034717ddb
s4474581 <s4474581@gmail.com>
2025-11-07 19:56:26 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
+      )}
+    </html>
+  )
+}
