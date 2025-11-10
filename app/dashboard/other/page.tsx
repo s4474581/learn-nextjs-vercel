60ca6e14234b610a1fd60e6eb7926aa235279cd7
s4474581 <s4474581@gmail.com>
2025-11-10 22:21:17 +0900

    disabled modal test

--- app/dashboard/other/page.tsx
+++ app/dashboard/other/page.tsx
@@ -3,7 +3,8 @@ import { ConfirmModal } from '@/app/ui/confirm-modal'
 export default function Page() {
   return (
     <div>
-      <ConfirmModal isOpen={true} />
+      <p>Confirm modal test</p>
+      {/* <ConfirmModal isOpen={true} /> */}
     </div>
   )
 }
