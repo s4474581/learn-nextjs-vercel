adebdfe34d0b5aa998e35ca59240300f35273e02
s4474581 <s4474581@gmail.com>
2025-11-10 22:18:29 +0900

    adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
+import { Metadata } from 'next'
+
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
 import { GoogleAnalytics } from '@next/third-parties/google'
 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard. built with App Router.',
+  metadataBase: new URL('https://learn-nextjs-vercel-panp.vercel.app/'),
+}
+
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode
 }) {
